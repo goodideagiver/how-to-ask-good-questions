1385a02d6e4e8f538db09830417418b78a163211
Karol <kariurek@gmail.com>
2022-08-30 15:24:29 +0200

    Updates

--- src/components/WindowLayout/WindowLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-import classes from './WindowLayout.module.scss';
-
-type Props = {
-	children: ReactNode;
-};
-export const WindowLayout = ({ children }: Props) => {
-	return <div className={classes.root}>{children}</div>;
-};
