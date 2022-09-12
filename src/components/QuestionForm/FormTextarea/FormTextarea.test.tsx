0d36a52ab0be7a81d2bd2f7732d789699dfa1580
Karol <kariurek@gmail.com>
2022-09-12 09:22:49 +0200

    Test: add test for FormTextarea
    to check if it renders proper label and textarea on passed props

--- src/components/QuestionForm/FormTextarea/FormTextarea.test.tsx
+++ src/components/QuestionForm/FormTextarea/FormTextarea.test.tsx
@@ -15,4 +15,36 @@ describe('FormTextarea', () => {
 		);
 		expect(container).toBeInTheDocument();
 	});
+
+	it('should have label with test content', () => {
+		const { getByText } = render(
+			<FormTextarea
+				label='test label'
+				objectKey='asd'
+				onChange={() => void 0}
+				placeholder='siem'
+				value='qwe'
+			/>
+		);
+		const textareaLabel = getByText('test label');
+
+		expect(textareaLabel).toBeInTheDocument();
+		expect(textareaLabel).toBeInstanceOf(HTMLLabelElement);
+	});
+
+	it('should have textarea with test content', () => {
+		const { getByText } = render(
+			<FormTextarea
+				label='test label'
+				objectKey='asd'
+				onChange={() => void 0}
+				placeholder='test placeholder'
+				value='test value'
+			/>
+		);
+		const textarea = getByText('test value');
+
+		expect(textarea).toBeInTheDocument();
+		expect(textarea).toBeInstanceOf(HTMLTextAreaElement);
+	});
 });
