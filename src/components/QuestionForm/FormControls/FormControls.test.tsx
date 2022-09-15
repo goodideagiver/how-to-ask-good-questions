acb65c1206f54f1c6cd60db573e175dfdc02df5f
prplblck <kariurek@gmail.com>
2022-09-15 22:02:11 +0200

    Test: add tests for FormControls buttons

---
+++ src/components/QuestionForm/FormControls/FormControls.test.tsx
@@ -0,0 +1,63 @@
+import { act, fireEvent, render, screen } from '@testing-library/react';
+import { FormControls } from './FormControls';
+
+describe('Form controls', () => {
+	it('should render reset button', () => {
+		const { getByText } = render(
+			<FormControls
+				resetFormHandler={() => void 0}
+				hasMessage={false}
+				message=''
+			/>
+		);
+		expect(getByText('Reset', { exact: false })).toBeInTheDocument();
+	});
+
+	it('should render copy button', () => {
+		const { getByText } = render(
+			<FormControls
+				resetFormHandler={() => void 0}
+				hasMessage={false}
+				message=''
+			/>
+		);
+		expect(getByText('Copy', { exact: false })).toBeInTheDocument();
+	});
+
+	it('should render language select', () => {
+		const { getByText } = render(
+			<FormControls
+				resetFormHandler={() => void 0}
+				hasMessage={false}
+				message=''
+			/>
+		);
+		expect(getByText('Language', { exact: false })).toBeInTheDocument();
+	});
+
+	it('should render copy button with default text', () => {
+		const { getByText } = render(
+			<FormControls
+				resetFormHandler={() => void 0}
+				hasMessage={false}
+				message=''
+			/>
+		);
+		expect(getByText('Copy', { exact: false })).toBeInTheDocument();
+	});
+
+	it('should render copy button with error text', () => {
+		render(
+			<FormControls
+				resetFormHandler={() => void 0}
+				hasMessage={false}
+				message=''
+			/>
+		);
+		const copyButton = screen.getByText('copy', { exact: false });
+
+		fireEvent.click(copyButton);
+
+		expect(copyButton).toHaveTextContent('error');
+	});
+});
