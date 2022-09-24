f99527793caca151aad1a2e6ff51cad400c8eb50
prplblck <kariurek@gmail.com>
2022-09-24 22:18:22 +0200

    Feat: add responsive rendering for FormProgress component
    to move the component to overlay root if it becomes an overlay on mobile

--- src/components/FormProgress/FormProgress.tsx
+++ src/components/FormProgress/FormProgress.tsx
@@ -1,21 +1,28 @@
+import { Portal } from '../../HOC/Portal/Portal';
 import classes from './FormProgress.module.scss';
 import { ProgressBar } from './ProgressBar';
 import { useFormClasses } from './useFormProgressClasses.hook';
+import { useIsMobile } from './useIsMobile.hook';
 
 type Props = {
 	percentage: number;
 };
 
 export const MAX_PERCENTAGE = 100;
+const MAX_PIXELS_WIDTH = 800;
 
 export const FormProgress = ({ percentage }: Props) => {
 	const { classNames } = useFormClasses(MAX_PERCENTAGE, percentage);
 
-	return (
+	const isMobile = useIsMobile(MAX_PIXELS_WIDTH);
+
+	const FormElement = (
 		<div className={classNames}>
 			<div className={classes.progWrapper}>
 				<ProgressBar percentage={percentage} />
 			</div>
 		</div>
 	);
+
+	return isMobile ? <Portal>{FormElement}</Portal> : FormElement;
 };
