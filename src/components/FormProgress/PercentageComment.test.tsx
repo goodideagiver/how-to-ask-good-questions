767bc340f0d133dba0665eefbaedbd73299ee452
prplblck <kariurek@gmail.com>
2022-09-26 17:59:05 +0200

    Test: update PercentageComment test to use module mock instead of any word regex search
    to more closely match real implementation

--- src/components/FormProgress/PercentageComment.test.tsx
+++ src/components/FormProgress/PercentageComment.test.tsx
@@ -1,17 +1,33 @@
 import { render, screen } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import { describe, expect, vi } from 'vitest';
 import { PercentageComment } from './PercentageComment';
 
+const COMPLETION_COMMENT = 'You have completed the form';
+const OVERCOMPLETION_COMMENT = 'You have overcompleted the form';
+
+vi.mock('react-i18next', () => ({
+	useTranslation: () => ({
+		t: (key: string) => {
+			if (key === 'completionComment') {
+				return COMPLETION_COMMENT;
+			}
+			if (key === 'overcompletionComment') {
+				return OVERCOMPLETION_COMMENT;
+			}
+		},
+	}),
+}));
+
 describe('PercentageComment component', () => {
-	test('renders completion comment', () => {
+	it('renders completion comment', () => {
 		render(<PercentageComment percentage={100} maxPercentage={100} />);
 
-		expect(screen.getByText(/([A-Z])\w+/)).toBeInTheDocument();
+		expect(screen.getByText(COMPLETION_COMMENT)).toBeInTheDocument();
 	});
 
-	test('renders completion comment', () => {
+	it('renders overcompletion comment', () => {
 		render(<PercentageComment percentage={120} maxPercentage={100} />);
 
-		expect(screen.getByText(/([A-Z])\w+/)).toBeInTheDocument();
+		expect(screen.getByText(OVERCOMPLETION_COMMENT)).toBeInTheDocument();
 	});
 });
