a91952ab076dc1046ca2ff67a53efdc586543629
Karol <kariurek@gmail.com>
2022-09-26 11:27:51 +0200

    Initial

--- src/components/FormProgress/FormProgress.test.tsx
+++ src/components/FormProgress/FormProgress.test.tsx
@@ -1,6 +1,12 @@
 import { render, screen } from '@testing-library/react';
-import { describe } from 'vitest';
+import { describe, vi } from 'vitest';
+import { OVERLAY_ROOT_DOM_ID } from '../../HOC/Portal/Portal';
 import { FormProgress } from './FormProgress';
+import { useIsMobile } from './useIsMobile.hook';
+
+vi.mock('./useIsMobile.hook', () => ({
+	useIsMobile: jest.fn(),
+}));
 
 describe('FormProgress', () => {
 	it('should have 0% width style', () => {
@@ -17,5 +23,17 @@ describe('FormProgress', () => {
 		const progressBar = screen.getByLabelText('progress bar');
 
 		expect(progressBar).toHaveStyle('width: 100%');
+	});
+
+	it('shuld render with portal when is mobile', () => {
+		render(<FormProgress percentage={100} />);
+
+		useIsMobile.mockReturnValueOnce(true);
+
+		const portal = document.getElementById(OVERLAY_ROOT_DOM_ID);
+		const progressBar = screen.getByLabelText('progress bar');
+
+		expect(progressBar).toBeInTheDocument();
+		expect(portal === null).toBe(false);
 	});
 });
