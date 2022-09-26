6fb71b4dd4d958ed80143aee599252e915faf27c
Karol <kariurek@gmail.com>
2022-09-26 14:58:26 +0200

    Test: change QuestionForm test
    to remove duplicate render statements

--- src/components/QuestionForm/QuestionForm.test.tsx
+++ src/components/QuestionForm/QuestionForm.test.tsx
@@ -2,10 +2,12 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, test } from 'vitest';
 import { QuestionForm } from './QuestionForm';
 
+beforeEach(() => {
+	render(<QuestionForm />);
+});
+
 describe('QuestionForm', () => {
 	test('inputs should initially be empty', () => {
-		render(<QuestionForm />);
-
 		const textarea = screen.getByRole('textbox');
 
 		expect(textarea).toBeInTheDocument();
@@ -14,16 +16,12 @@ describe('QuestionForm', () => {
 	});
 
 	test('only one input should be visible', () => {
-		render(<QuestionForm />);
-
 		const textareas = screen.getAllByRole('textbox');
 
 		expect(textareas).toHaveLength(1);
 	});
 
 	it('should have 2 buttons', () => {
-		render(<QuestionForm />);
-
 		const expectedButtonsAmount = 2;
 
 		const buttons = screen.getAllByRole('button');
@@ -32,8 +30,6 @@ describe('QuestionForm', () => {
 	});
 
 	test('should have 2 textareas if first textarea has text', () => {
-		render(<QuestionForm />);
-
 		const textarea = screen.getByRole('textbox') as HTMLTextAreaElement;
 
 		fireEvent.change(textarea, { target: { value: 'test' } });
