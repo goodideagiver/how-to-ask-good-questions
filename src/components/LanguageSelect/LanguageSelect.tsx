4673ce407cbfe8b617c04c13c91bfb979e9a3f46
Karol <kariurek@gmail.com>
2022-10-26 08:11:16 +0200

    Chore: add radix select

--- src/components/LanguageSelect/LanguageSelect.tsx
+++ src/components/LanguageSelect/LanguageSelect.tsx
@@ -1,36 +1,58 @@
 /* eslint-disable react/no-unknown-property */
-import { useTranslation } from 'react-i18next';
-import { DECLARED_LANGUAGES } from '../../config';
+import { useTranslation } from 'react-i18next'
+import { DECLARED_LANGUAGES } from '../../config'
 
-import classes from './LanguageSelect.module.scss';
-import { useLanguageSelect } from './useLanguageSelect';
+import * as Select from '@radix-ui/react-select'
+
+import { useLanguageSelect } from './useLanguageSelect'
 
 export const LanguageSelect = () => {
-	const { t } = useTranslation();
+  const { t } = useTranslation()
 
-	const { selectHandler, selectRef } = useLanguageSelect();
+  const { selectHandler, selectRef } = useLanguageSelect()
 
-	const LanguageOptions = DECLARED_LANGUAGES.map((lang) => (
-		<option key={lang} value={lang}>
-			{lang}
-		</option>
-	));
+  const LanguageOptions = DECLARED_LANGUAGES.map((lang) => (
+    <Select.Item value={lang} key={lang}>
+      <Select.ItemText>{lang}</Select.ItemText>
+    </Select.Item>
+  ))
 
-	return (
-		<div>
-			<label htmlFor='lang-select'>{t('chooseLanguage')}:</label>
-			<select
-				ref={selectRef}
-				className={classes.input}
-				onChange={selectHandler}
-				name='languages'
-				id='lang-select'
-			>
-				<option disabled value=''>
-					--Please choose an option--
-				</option>
-				{LanguageOptions}
-			</select>
-		</div>
-	);
-};
+  return (
+    <Select.Root>
+      <Select.Trigger aria-label='language select'>
+        <Select.Value placeholder={t('chooseLanguage')} />
+        <Select.Icon />
+      </Select.Trigger>
+      <Select.Portal>
+        <Select.Content>
+          <Select.ScrollUpButton />
+          <Select.Viewport>{LanguageOptions}</Select.Viewport>
+          <Select.ScrollDownButton />
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
+  )
+}
+
+// return (
+//   <div>
+//     <label htmlFor='lang-select'>{t('chooseLanguage')}:</label>
+//     <Select></Select>{' '}
+//     <option disabled value=''>
+//       --Please choose an option--
+//     </option>
+//     {LanguageOptions}
+//     <select
+//       ref={selectRef}
+//       className={classes.input}
+//       onChange={selectHandler}
+//       name='languages'
+//       id='lang-select'
+//     >
+//       <option disabled value=''>
+//         --Please choose an option--
+//       </option>
+//       {LanguageOptions}
+//     </select>
+//   </div>
+// )
