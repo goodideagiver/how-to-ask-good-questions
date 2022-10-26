4bcff8bc8a7a600ad89d86609be3c6893376058d
Karol <kariurek@gmail.com>
2022-10-26 11:10:17 +0200

    Feat: add styles and functionality to select lang

--- src/components/LanguageSelect/LanguageSelect.tsx
+++ src/components/LanguageSelect/LanguageSelect.tsx
@@ -4,32 +4,62 @@ import { DECLARED_LANGUAGES } from '../../config'
 
 import * as Select from '@radix-ui/react-select'
 
+import { styled } from '@stitches/react'
 import { useLanguageSelect } from './useLanguageSelect'
+
+const StyledContent = styled(Select.Content, {
+  width: '100%',
+  backgroundColor: 'rgb(44, 47, 51)',
+  borderRadius: '8px',
+  border: '1px solid #121212',
+  boxShadow: '0 0 5px #121212',
+  overflow: 'hidden',
+})
+
+const StyledItem = styled(Select.Item, {
+  padding: '1rem 2rem',
+  backgroundColor: 'rgb(44, 47, 51)',
+  color: 'white',
+  cursor: 'pointer',
+  '&:hover': {
+    backgroundColor: '#202225',
+    color: 'white',
+    outline: 'none',
+    border: 'none',
+  },
+})
 
 export const LanguageSelect = () => {
   const { t } = useTranslation()
 
-  const { selectHandler, selectRef } = useLanguageSelect()
+  const { selectHandler, selectValue } = useLanguageSelect()
 
   const LanguageOptions = DECLARED_LANGUAGES.map((lang) => (
-    <Select.Item value={lang} key={lang}>
-      <Select.ItemText>{lang}</Select.ItemText>
-    </Select.Item>
+    <StyledItem value={lang} key={lang}>
+      <Select.ItemText>{lang.toUpperCase()}</Select.ItemText>
+    </StyledItem>
   ))
 
+  const SelectContent = (
+    <StyledContent>
+      <Select.ScrollUpButton />
+      <Select.Viewport>
+        <Select.Group>
+          <Select.Label>{t('chooseLanguage')}</Select.Label>
+          {LanguageOptions}
+        </Select.Group>
+      </Select.Viewport>
+      <Select.ScrollDownButton />
+    </StyledContent>
+  )
+
   return (
-    <Select.Root>
+    <Select.Root onValueChange={selectHandler} value={selectValue}>
       <Select.Trigger aria-label='language select'>
         <Select.Value placeholder={t('chooseLanguage')} />
         <Select.Icon />
       </Select.Trigger>
-      <Select.Portal>
-        <Select.Content>
-          <Select.ScrollUpButton />
-          <Select.Viewport>{LanguageOptions}</Select.Viewport>
-          <Select.ScrollDownButton />
-        </Select.Content>
-      </Select.Portal>
+      {SelectContent}
     </Select.Root>
   )
 }
