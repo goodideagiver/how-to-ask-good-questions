e0799af41717d6ceda2e131ffc311e0fc2c1324d
prplblck <kariurek@gmail.com>
2022-10-29 09:01:35 +0200

    Feat: add stick to top progress bar on mobile

--- src/components/FormProgress/FormProgress.tsx
+++ src/components/FormProgress/FormProgress.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { Portal } from '../../HOC/Portal/Portal'
 import classes from './FormProgress.module.scss'
 import { formClassesHelper } from './formProgressClasses.helper'
@@ -12,12 +13,38 @@ export const MAX_PERCENTAGE = 100
 export const MAX_PIXELS_WIDTH = 800
 
 export const FormProgress = ({ percentage }: Props) => {
+  const [progressTopPosition, setProgressTopPosition] = useState('0')
   const { classNames } = formClassesHelper(MAX_PERCENTAGE, percentage)
 
   const isMobile = useIsMobile(MAX_PIXELS_WIDTH)
 
+  useEffect(() => {
+    if (!isMobile) return
+    const scrollHandler = () => {
+      const correctionAmount = window.visualViewport?.offsetTop || 0
+
+      setProgressTopPosition((prev) => {
+        if (Number(prev) === correctionAmount) return prev
+        return correctionAmount.toFixed(0)
+      })
+    }
+    window.addEventListener('scroll', scrollHandler)
+    const interval = setInterval(scrollHandler, 100)
+    return () => {
+      window.removeEventListener('scroll', scrollHandler)
+      clearInterval(interval)
+    }
+  }, [isMobile])
+
   const Progress = (
-    <div className={classNames}>
+    <div
+      className={classNames}
+      style={{
+        // transform: `translate3d: 0 ${progressTopPosition}px 0;`,
+        transform: `translate3d(0, ${progressTopPosition}px, 0)`,
+        transition: '0.1s ease-in-out',
+      }}
+    >
       <div className={classes.progWrapper}>
         <ProgressBar percentage={percentage} />
       </div>
