7cb3139af219b119a6b66373d978bb1be34a9418
prplblck <kariurek@gmail.com>
2022-10-30 06:56:24 +0100

    Test: update language select test
    to make it work on updated code

--- src/components/QuestionForm/FormControls/FormControls.test.tsx
+++ src/components/QuestionForm/FormControls/FormControls.test.tsx
@@ -1,63 +1,64 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import { FormControls } from './FormControls';
+import { fireEvent, render, screen } from '@testing-library/react'
+import { FormControls } from './FormControls'
 
 describe('Form controls', () => {
-	it('should render reset button', () => {
-		const { getByText } = render(
-			<FormControls
-				resetFormHandler={() => void 0}
-				hasMessage={false}
-				message=''
-			/>
-		);
-		expect(getByText('Reset', { exact: false })).toBeInTheDocument();
-	});
+  it('should render reset button', () => {
+    const { getByText } = render(
+      <FormControls
+        resetFormHandler={() => void 0}
+        hasMessage={false}
+        message=''
+      />
+    )
+    expect(getByText('Reset', { exact: false })).toBeInTheDocument()
+  })
 
-	it('should render copy button', () => {
-		const { getByText } = render(
-			<FormControls
-				resetFormHandler={() => void 0}
-				hasMessage={false}
-				message=''
-			/>
-		);
-		expect(getByText('Copy', { exact: false })).toBeInTheDocument();
-	});
+  it('should render copy button', () => {
+    const { getByText } = render(
+      <FormControls
+        resetFormHandler={() => void 0}
+        hasMessage={false}
+        message=''
+      />
+    )
+    expect(getByText('Copy', { exact: false })).toBeInTheDocument()
+  })
 
-	it('should render language select', () => {
-		const { getByText } = render(
-			<FormControls
-				resetFormHandler={() => void 0}
-				hasMessage={false}
-				message=''
-			/>
-		);
-		expect(getByText('Language', { exact: false })).toBeInTheDocument();
-	});
+  it('should render language select', () => {
+    render(
+      <FormControls
+        resetFormHandler={() => void 0}
+        hasMessage={false}
+        message=''
+      />
+    )
+    const selectElement = screen.getByLabelText(/language/i)
+    expect(selectElement).toBeInTheDocument()
+  })
 
-	it('should render copy button with default text', () => {
-		const { getByText } = render(
-			<FormControls
-				resetFormHandler={() => void 0}
-				hasMessage={false}
-				message=''
-			/>
-		);
-		expect(getByText('Copy', { exact: false })).toBeInTheDocument();
-	});
+  it('should render copy button with default text', () => {
+    const { getByText } = render(
+      <FormControls
+        resetFormHandler={() => void 0}
+        hasMessage={false}
+        message=''
+      />
+    )
+    expect(getByText('Copy', { exact: false })).toBeInTheDocument()
+  })
 
-	it('should render copy button with error text', () => {
-		render(
-			<FormControls
-				resetFormHandler={() => void 0}
-				hasMessage={false}
-				message=''
-			/>
-		);
-		const copyButton = screen.getByText('copy', { exact: false });
+  it('should render copy button with error text', () => {
+    render(
+      <FormControls
+        resetFormHandler={() => void 0}
+        hasMessage={false}
+        message=''
+      />
+    )
+    const copyButton = screen.getByText('copy', { exact: false })
 
-		fireEvent.click(copyButton);
+    fireEvent.click(copyButton)
 
-		expect(copyButton).toHaveTextContent(/error/i);
-	});
-});
+    expect(copyButton).toHaveTextContent(/error/i)
+  })
+})
