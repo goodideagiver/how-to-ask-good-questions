ecabc6a52f557223d93324a63647257c727f3f04
prplblck <kariurek@gmail.com>
2022-11-01 13:36:18 +0100

    Feat: add copy to clipboard on output click

--- src/components/QuestionForm/FormOutput/FormOutput.tsx
+++ src/components/QuestionForm/FormOutput/FormOutput.tsx
@@ -1,6 +1,8 @@
+import { useToast } from '@chakra-ui/toast'
 import { useTranslation } from 'react-i18next'
 import ReactMarkdown from 'react-markdown'
 import { cssBool, cssClass } from '../../../helpers/cssClass.helper'
+import { useMessageStore } from '../../../store/message-store'
 
 import classes from './FormOutput.module.scss'
 
@@ -11,15 +13,43 @@ type Props = {
 
 export const FormOutput = ({ hasMessage, message }: Props) => {
   const { t } = useTranslation()
+  const toast = useToast()
+
+  const formattedMessage = useMessageStore((state) => state.formattedMessage)
 
   const outputCss = cssClass(classes.right, cssBool(!message, classes.hidden))
+
+  const copyButtonHandler = async () => {
+    try {
+      await navigator.clipboard.writeText(formattedMessage())
+      toast({
+        title: t('copyMessage.success.title'),
+        description: t('copyMessage.success.message'),
+        status: 'success',
+        duration: 2000,
+        isClosable: true,
+      })
+    } catch (error) {
+      toast({
+        title: t('copyMessage.error.title'),
+        description: t('copyMessage.error.message'),
+        status: 'error',
+        duration: 2000,
+        isClosable: true,
+      })
+    }
+  }
 
   return (
     <div className={outputCss}>
       <p className={classes['output-title']}>{t('outputPreview')}:</p>
       <div className={classes.output}>
         {hasMessage ? (
-          <ReactMarkdown className={classes.markdown}>{message}</ReactMarkdown>
+          <button onClick={copyButtonHandler} className={classes.button}>
+            <ReactMarkdown className={classes.markdown}>
+              {message}
+            </ReactMarkdown>
+          </button>
         ) : (
           <p className={classes.empty}>{t('noData')}</p>
         )}
